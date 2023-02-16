654febca02d7dbfa2e6e25b1968cd8ee1f60606e
Vitor Silva <vitor.silva@ubistart.com>
2023-02-16 13:16:34 -0300

    feat: install chart lib

---
+++ src/components/FooterBar/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, Text } from './styles';
+
+const FooterBar: React.FC = () => {
+  return (
+    <Container>
+      <Text>Olá Mundo</Text>
+    </Container>
+  );
+}
+
+export default FooterBar;
